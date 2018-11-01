11937930bbff920dd6d022bfc028b03f622dbaae
Aram Danielyan <aram@aarki.com>
2018-11-01 12:50:43 +0400

    make gchat notification simple text

--- graphite_beacon/handlers/gchat.py
+++ graphite_beacon/handlers/gchat.py
@@ -1,11 +1,11 @@
 import json
+from datetime import datetime
 
 from tornado import httpclient as hc
 from tornado import gen
 
 from graphite_beacon.handlers import LOGGER, AbstractHandler
 from graphite_beacon.template import TEMPLATES
-
 
 class GChatHandler(AbstractHandler):
 
@@ -16,13 +16,6 @@ class GChatHandler(AbstractHandler):
         'webhook': None,
     }
 
-    colors = {
-        'red': '#A12830',
-        'green': '#3F704D',
-        'blue': '#3E67CE',
-        'yellow': '#FFAC00',
-    }
-
     def init_handler(self):
         self.webhook = self.options.get('webhook')
         assert self.webhook, 'GChat webhook is not defined.'
@@ -31,34 +24,20 @@ class GChatHandler(AbstractHandler):
     def get_short(self, level, alert, value, target=None, ntype=None, rule=None):  # pylint: disable=unused-argument
         tmpl = TEMPLATES[ntype]['gchat']
         return tmpl.generate(
-            level=level, reactor=self.reactor, alert=alert, value=value, target=target, colors=self.colors).strip()
+            level=level, reactor=self.reactor, alert=alert, value=value, target=target).strip()
 
     @gen.coroutine
     def notify(self, level, *args, **kwargs):
         LOGGER.debug("Handler (%s) %s", self.name, level)
 
         message = self.get_short(level, *args, **kwargs).decode('UTF-8')
-        data = {
-            "cards": [
-                {
-                    "sections": [
-                        {
-                            "widgets": [
-                                {
-                                    "textParagraph": {
-                                        "text": message
-                                    }
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }
+        data = {'text': message}
+
+        webhook = self.webhook.replace('{{CURRENT_DAY}}', datetime.utcnow().strftime("%Y%m%d"))
 
         body = json.dumps(data)
         yield self.client.fetch(
-            self.webhook,
+            webhook,
             method='POST',
             headers={'Content-Type': 'application/json; charset=UTF-8'},
             body=body
